94c5d8b5bb32891ec99bb53bac2592ecf3af332e
pigako <50404311+pigako@users.noreply.github.com>
2021-02-16 12:38:04 +0900

    로깅 라이브러리 추가 및 prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as morgan from "morgan";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {});
+    app.setGlobalPrefix("api/v1");
+    app.use(morgan("dev"));
     await app.listen(4000);
 }
 bootstrap();
