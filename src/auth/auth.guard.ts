5c3ae05c1bd84673f10e89fc58cc47ccef4b5b8e
pigako <50404311+pigako@users.noreply.github.com>
2021-02-17 12:05:44 +0900

    auth guard

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        // const request = context.switchToHttp().getRequest();
+
+        // if (request?.body?.access) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+
+        console.log(context);
+        return false;
+    }
+}
