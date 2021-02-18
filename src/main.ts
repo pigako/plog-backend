4ed5ecf56894514e0a74008042aa9228d81fa257
pigako <50404311+pigako@users.noreply.github.com>
2021-02-18 15:44:25 +0900

    CORS 설정 추가, validation check, helmet 설정

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,30 @@ import { AppModule } from "./app.module";
 import * as morgan from "morgan";
 import * as cookieParser from "cookie-parser";
 import * as session from "express-session";
+import * as helmet from "helmet";
 import { ConfigService } from "@nestjs/config";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {});
     const configService = app.get(ConfigService);
 
     app.setGlobalPrefix("api/v1");
-    app.enableCors();
+    app.enableCors({
+        allowedHeaders: "Content-Type",
+        methods: "POST,GET,PUT,PATCH,DELETE,OPTIONS",
+        credentials: true,
+        origin: process.env.NODE_ENV === "production" ? "https://www.pigako.com" : true
+    });
+    app.useGlobalPipes(new ValidationPipe());
+    app.use(helmet());
     app.use(morgan("dev"));
     app.use(cookieParser());
     app.use(
         session({
             resave: false,
             saveUninitialized: false,
-            secret: configService.get("SECRET_KEY"),
+            secret: configService.get("SESSION_KEY"),
             cookie: {
                 httpOnly: true,
                 secure: true
