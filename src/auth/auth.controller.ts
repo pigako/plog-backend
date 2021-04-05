24c8f7d8ef9c8bbb870b98c3ddb3563808faef83
pigako <50404311+pigako@users.noreply.github.com>
2021-04-05 14:01:53 +0900

    Auth Controller Api Docs 제거

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, HttpCode, Query, Redirect, Req, Res } from "@nestjs/common";
-import { Request, Response } from "express";
+import { Controller, Get, HttpCode, HttpStatus, Query, Redirect, Req, Res } from "@nestjs/common";
+import { ApiExcludeEndpoint, ApiTags } from "@nestjs/swagger";
+import { Response } from "express";
 import { AuthService } from "./auth.service";
 
+@ApiTags("Auth")
 @Controller("auth")
 export class AuthController {
     constructor(private readonly service: AuthService) {}
+
+    @ApiExcludeEndpoint(true)
     @Get("google/callback")
-    @HttpCode(302)
+    @HttpCode(HttpStatus.MOVED_PERMANENTLY)
     async googleCallback(@Res() response: Response, @Query("code") code: string) {
         const authResult = await this.service.googleAuthLogin(code);
 
@@ -27,8 +31,9 @@ export class AuthController {
         response.redirect(process.env.NODE_ENV === "production" ? "https://www.pigako.com/blog/posts" : "http://localhost:3000/blog/posts");
     }
 
+    @ApiExcludeEndpoint(true)
     @Get("kakao/callback")
-    @Redirect("https://www.pigako.com/blog/posts", 302)
+    @Redirect("https://www.pigako.com/blog/posts", HttpStatus.MOVED_PERMANENTLY)
     async kakakoCallback(@Res() response: Response, @Query("code") code: string) {
         const authResult = await this.service.kakaoAuthLogin(code);
 
@@ -51,8 +56,9 @@ export class AuthController {
         }
     }
 
+    @ApiExcludeEndpoint(true)
     @Get("github/callback")
-    @Redirect("https://www.pigako.com/blog/posts", 302)
+    @Redirect("https://www.pigako.com/blog/posts", HttpStatus.MOVED_PERMANENTLY)
     async githubCallback(@Res() response: Response, @Query("code") code: string) {
         const authResult = await this.service.githubAuthLogin(code);
 
@@ -75,8 +81,9 @@ export class AuthController {
         }
     }
 
+    @ApiExcludeEndpoint(true)
     @Get("naver/callback")
-    @Redirect("https://www.pigako.com/blog/posts", 302)
+    @Redirect("https://www.pigako.com/blog/posts", HttpStatus.MOVED_PERMANENTLY)
     async naverCallback(@Res() response: Response, @Query("code") code: string, @Query("state") state: string) {
         const authResult = await this.service.naverAuthLogin(code, state);
 
